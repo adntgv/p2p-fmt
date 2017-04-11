14cc725e88878dd04e7606986bf341c039dddabc
right-hearted <right-hearted@yandex.ru>
2017-04-11 22:41:38 +0600

    First commit

--- client.py
+++ client.py
@@ -6,6 +6,9 @@ import os
 import json
 
 #command line arguments
+HOST = 'localhost'
+PORT = 2101
+
 s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 s.connect(('8.8.8.8',80))
 ip, port = s.getsockname()
@@ -22,7 +25,6 @@ class Client:
         self.files = []
         self.fnum = 0
         for fl in files:
-
             self.fnum= self.fnum + 1
             fname, ext = os.path.splitext(fl)
             fl = os.path.join(folder,fl)
@@ -38,24 +40,31 @@ class Client:
                 'port':port
             }
             self.files.append(f_dict)
+        print("Local files: ")
+        print(files)
 
     def connect(self,host,port):
+        print('Connecting')
         self.sock.connect((host,port))
-        self.sock.send(bytes("HELLO","utf-8"))
+        self.send("HELLO")
+        print('sent hello')
         din = self.recieve()
         strin = str(din,"utf-8")
         if strin != 'HI':
             print("Incorrect connection starter")
-            self.sock.disconnect()
+            self.disconnect()
         self.share()
+        print('Shared')
 
     def share(self):
-        self.send(bytes('SHARE',"utf-8"))
-        self.send(bytes(json.dumps(self.files),"utf-8"))
-        self.send(bytes('DONE',"utf-8"))
+        self.send('SHARE')
+        tosend = json.dumps(self.files)
+        print(tosend)
+        self.send(tosend)
+        self.send('DONE')
 
     def send(self,data):
-        self.sock.send(data)
+        self.sock.send(bytes(data,'utf-8'))
 
     def recieve(self):
         return self.sock.recv(1024)
@@ -64,22 +73,23 @@ class Client:
         self.sock.disconnect()
 
     def operate(self):
-        pass
+        command = None
+        while command != 'EXIT':
+            command = input("Command ('?' for help) :")#SEARCH, LISt
+            if 'SEARCH' in command:
+
 
     def bye(self):
         pass
 
-if __name__ == "__main__":
-    HOST, PORT = "localhost", 2101
-    client = Client(folder='shared')
-    client.connect(HOST,PORT)
-    client.operate()
-    client.bye()
+client = Client(folder='shared')
+client.connect(HOST,PORT)
+client.operate()
+client.bye()
 
 # define path to shared folder:
 #   by config file
 #   by command line arguments
-# send list to server
 # search for a $songname in fmt
 # request a file from peer
 # finish BYE
