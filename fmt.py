116a5580de980fae832bb20f77735ea1fa6983b7
right-hearted <right-hearted@yandex.ru>
2017-04-11 22:42:11 +0600

    First commit

--- fmt.py
+++ fmt.py
@@ -1,49 +1,75 @@
 #!/usr/bin/env python3.5
 # -*- coding: utf-8 -*-
 
-import socketserver as ss
+import socket
 import json
+import re
+import selectors
 
+sel = selectors.DefaultSelector()
 
 fileslist = []
-class TCPHandler(ss.BaseRequestHandler):
-    def handle(self):
-        self.verify()
-        while 1:
-            strin = str(self.recieve(),"utf-8")
-            if strin == 'SHARE':
-                self.addPeer()
-            elif strin == 'SEARCH':
-                self.search()
-            elif strin == 'BYE':
-                self.removePeer()
+
+def accept(sock, mask):
+    conn, addr = sock.accept()  # Should be ready
+    print('accepted', conn)
+    conn.setblocking(False)
+    sel.register(conn, selectors.EVENT_READ, verify)
+
+def main_loop(conn, mask):
+    print('In main loop')
+    strin = str(conn.recv(1024).strip(),"utf-8")
+    print('STRIN : '+strin)
+    if 'SHARE' in strin:
+        sel.modify(conn, selectors.EVENT_READ, addPeer)
+    elif 'SEARCH' in strin:
+        sel.modify(conn, selectors.EVENT_READ, search)
+    elif 'BYE' in strin:
+        sel.modify(conn, selectors.EVENT_READ, removePeer)
+    elif 'HELLO' in strin:
+        conn.send(bytes("HI","utf-8"))
+    else:
+        pass
+
+def addPeer(conn,mask):
+    chunks = ""
+    chunk = ""
+    while not 'DONE' in chunk:
+        chunk = str(conn.recv(1024).strip(),'utf-8')
+        chunks = chunks+chunk
+        chunks = re.sub(re.escape('DONE'), '', chunks)
+    lst = json.loads(chunks)
+    for fl in lst:
+        fileslist.append(fl)
+    print(fileslist)
+    sel.modify(conn, selectors.EVENT_READ, main_loop)
+
+def search(conn,mask):
+    
+    sel.modify(conn, selectors.EVENT_READ, main_loop)
+
+def verify(conn,mask):
+    strin = str(conn.recv(1024).strip(),"utf-8")
+
+    if(strin != "HELLO"):
+        print("Incorrect connection starter")
+        sel.unregister(conn)
+        conn.close()
+    else:
+        print('verify STRIN : '+strin)
+        conn.send(bytes("HI","utf-8"))
+        sel.modify(conn, selectors.EVENT_READ, main_loop)
 
 
-    def addPeer(self):
-        files = json.loads(str(self.recieve(),'utf-8'))
-        print(files)
-        # for x in range(count):
-        #     strin = str(self.recieve(),"utf-8")
-        #     fileslist.append(json.loads(strin))
-        #print(fileslist)
+sock = socket.socket()
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+sock.bind(('localhost', 2101))
+sock.listen(100)
+sock.setblocking(False)
+sel.register(sock, selectors.EVENT_READ, accept)
 
-    def verify(self):
-        strin = str(self.recieve(),"utf-8")
-        if(strin != "HELLO"):
-            print("Incorrect connection starter")
-            self.sock.close()
-        self.send(bytes("HI","utf-8"))
-
-    def recieve(self):
-        return self.request.recv(1024).strip()
-
-    def send(self,data):
-        return self.request.sendall(data)
-
-
-
-if __name__ == "__main__":
-    HOST, PORT = "localhost", 2101
-    server = ss.TCPServer((HOST,PORT),TCPHandler)
-    print("serving at port:" ,PORT)
-    server.serve_forever()
+while True:
+    events = sel.select()
+    for key, mask in events:
+        callback = key.data
+        callback(key.fileobj, mask)
