41a2839754c49bfe5b6b9abbd6e9e658376a1e44
right-hearted <right-hearted@yandex.ru>
2017-04-15 03:40:11 +0600

    изменено:      client.py 	изменено:      fmt.py

--- client.py
+++ client.py
@@ -4,11 +4,20 @@
 import socket
 import os
 import json
+import sys
+import re
+import selectors
+import time
+import threading
 
-#command line arguments
+SEL = selectors.DefaultSelector()
+
 HOST = 'localhost'
 PORT = 2101
-
+MSIZE = 1024
+FILES = []
+FOLDER = None
+LPORT = None
 s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 s.connect(('8.8.8.8',80))
 ip, port = s.getsockname()
@@ -21,10 +30,10 @@ class Client:
         else:
             self.sock = sock
 
-        files = os.listdir(folder)
+        FILES = os.listdir(folder)
         self.files = []
         self.fnum = 0
-        for fl in files:
+        for fl in FILES:
             self.fnum= self.fnum + 1
             fname, ext = os.path.splitext(fl)
             fl = os.path.join(folder,fl)
@@ -37,59 +46,136 @@ class Client:
                 'fsize': size,
                 'fmod': time,#(DD/MM/YY),
                 'ip':ip,
-                'port':port
+                'port':LPORT
             }
             self.files.append(f_dict)
         print("Local files: ")
-        print(files)
+        print(FILES)
 
-    def connect(self,host,port):
+        SEL.register(self.sock, selectors.EVENT_READ, self.connect)
+
+    def connect(self,conn,mask):
         print('Connecting')
-        self.sock.connect((host,port))
-        self.send("HELLO")
-        print('sent hello')
-        din = self.recieve()
-        strin = str(din,"utf-8")
+        self.sock.connect((HOST,PORT))
+        self.say("HELLO")
+        strin = self.lstn()
         if strin != 'HI':
             print("Incorrect connection starter")
-            self.disconnect()
+            self.sock.close()
         self.share()
-        print('Shared')
+        SEL.modify(conn, selectors.EVENT_WRITE, self.operate)
 
     def share(self):
-        self.send('SHARE')
+        self.say('SHARE')
         tosend = json.dumps(self.files)
-        print(tosend)
-        self.send(tosend)
-        self.send('DONE')
+        self.say(tosend)
+        self.say('DONE')
 
-    def send(self,data):
-        self.sock.send(bytes(data,'utf-8'))
+    def say(self,data):
+        self.sock.sendall(bytes(data,'utf-8'))
+
+    def lstn(self):
+        return str(self.sock.recv(MSIZE),'utf-8')
 
     def recieve(self):
-        return self.sock.recv(1024)
+        sz = 1024
+        rec = []
+        while sz==1024:
+            data = self.sock.recv(1024)
+            sz = len(data)
+            print(sz)
+            rec = rec.append(data)
+        return data
 
     def disconnect(self):
-        self.sock.disconnect()
-
-    def operate(self):
-        command = None
-        while command != 'EXIT':
-            command = input("Command ('?' for help) :")#SEARCH, LISt
-            if 'SEARCH' in command:
-
+        self.sock.close()
 
     def bye(self):
-        pass
+        self.say("BYE")
+        self.sock.close()
 
-client = Client(folder='shared')
-client.connect(HOST,PORT)
-client.operate()
-client.bye()
+    def download(self,fname,rip,rport):
+        print(fname)
+        print(rip)
+        print(rport)
+        with open(os.path.join(FOLDER,fname), 'wb') as fl:
+            tmp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            tmp.connect((rip,int(rport)))
+            sz = 1024
+            rec = []
+            tmp.send(bytes(fname,'utf-8'))
+            while sz==1024:
+                data = tmp.recv(1024)
+                sz = len(data)
+                fl.write(data)
+            fl.close()
+            tmp.close()
 
+    def operate(self,conn,mask):
+        inp = None
+        while inp != 'EXIT':
+            inp = input("Command ('?' for help) :")#SEARCH, LIST
+            if 'SEARCH' in inp:
+                self.say('SEARCH')
+                time.sleep(1)
+                self.say(inp.replace('SEARCH ',''))
+                ret = json.loads(self.lstn())
+                print(ret)
+            elif 'LIST' in inp:
+                self.say('LIST')
+                data = json.loads(self.lstn())
+                for entity in data:
+                    print(entity)
+            elif 'DOWNLOAD' in inp:
+                op = inp.split(',')
+                self.download(op[1],op[2],op[3])
+            elif 'EXIT' in inp:
+                self.bye()
+            elif '?' in inp:
+                print("SEARCH (filename)  -  Search for a file in servers lists")
+                print("LIST (filename) - List all the files that server has")
+                print("DOWNLOAD;(filename);(ip);(port) - Download a file from given host")
+                print("EXIT - to exit")
+
+class theThread(threading.Thread):
+    def run(self):
+        sock = socket.socket()
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.bind((ip, LPORT+1))
+        print("Started serving on port ")
+        print( LPORT+1)
+        sock.listen(5)
+        tosend = []
+        while True:
+            print("Waiting for connections")
+            conn, _ = sock.accept()
+            print("Connnected to")
+            print(conn)
+            fname = str(conn.recv(1024).strip(),"utf-8")
+            fl = open(os.path.join(sys.argv[1],fname),'rb')
+            l = fl.read(1024)
+            while (l):
+                conn.send(l)
+                l = fl.read(1024)
+            fl.close()
+            conn.close()
+
+def main(fldr):
+    client = Client(folder=fldr[0])
+    theThread().start()
+    while True:
+        evts = SEL.select()
+        for key, mask in evts:
+            callback = key.data
+            callback(key.fileobj, mask)
+
+if __name__ == '__main__':
+    LPORT = int(sys.argv[2])
+    FOLDER = sys.argv[1]
+    main(sys.argv[1:])
 # define path to shared folder:
 #   by config file
 #   by command line arguments
-# search for a $songname in fmt
+# search for a $filename in fmt
 # request a file from peer
 # finish BYE
--- fmt.py
+++ fmt.py
@@ -1,31 +1,38 @@
 #!/usr/bin/env python3.5
 # -*- coding: utf-8 -*-
 
-import socket
 import json
 import re
+import socket
+
 import selectors
 
-sel = selectors.DefaultSelector()
+SEL = selectors.DefaultSelector()
 
-fileslist = []
+FILELIST = []
 
-def accept(sock, mask):
-    conn, addr = sock.accept()  # Should be ready
+def accept(l_sock,mask):
+    """Accept connections and send to verify."""
+    conn, _ = l_sock.accept()  # Should be ready
     print('accepted', conn)
     conn.setblocking(False)
-    sel.register(conn, selectors.EVENT_READ, verify)
+    SEL.register(conn, selectors.EVENT_READ, verify)
 
-def main_loop(conn, mask):
+def main_loop(conn,mask):
+    """Main loop routes actions."""
     print('In main loop')
-    strin = str(conn.recv(1024).strip(),"utf-8")
+    strin = str(conn.recv(1024).strip(), "utf-8")
     print('STRIN : '+strin)
     if 'SHARE' in strin:
-        sel.modify(conn, selectors.EVENT_READ, addPeer)
+        SEL.modify(conn, selectors.EVENT_READ, addPeer)
     elif 'SEARCH' in strin:
-        sel.modify(conn, selectors.EVENT_READ, search)
+        SEL.modify(conn, selectors.EVENT_READ, search)
+    elif 'LIST' in strin:
+        lst(conn,mask)
     elif 'BYE' in strin:
-        sel.modify(conn, selectors.EVENT_READ, removePeer)
+        for fl in FILELIST:
+            if fl[ip]==conn[ip]:
+                FILELIST.remove(fl)
     elif 'HELLO' in strin:
         conn.send(bytes("HI","utf-8"))
     else:
@@ -40,36 +47,54 @@ def addPeer(conn,mask):
         chunks = re.sub(re.escape('DONE'), '', chunks)
     lst = json.loads(chunks)
     for fl in lst:
-        fileslist.append(fl)
-    print(fileslist)
-    sel.modify(conn, selectors.EVENT_READ, main_loop)
+        FILELIST.append(fl)
+    print(FILELIST)
+    SEL.modify(conn, selectors.EVENT_READ, main_loop)
 
 def search(conn,mask):
-    
-    sel.modify(conn, selectors.EVENT_READ, main_loop)
+    strin = str(conn.recv(1024).strip(), "utf-8")
+    tosend = []
+    print("Searching for " + strin)
+    for filedesc in FILELIST:
+        for field in filedesc:
+            print(field)
+            if strin == filedesc[field]:
+                tosend.append(filedesc)
+                print("Found " + field)
+    if len(tosend) == 0 :
+        conn.send(bytes(json.dumps("NOT FOUND"),"utf-8"))
+    else:
+        conn.send(bytes(json.dumps(tosend),"utf-8"))
+    SEL.modify(conn, selectors.EVENT_READ, main_loop)
+
+def lst(conn,mask):
+    print("in list")
+    conn.send(bytes(json.dumps(FILELIST),"utf-8"))
+    print("listed")
+    SEL.modify(conn, selectors.EVENT_READ, main_loop)
+
 
 def verify(conn,mask):
     strin = str(conn.recv(1024).strip(),"utf-8")
 
     if(strin != "HELLO"):
         print("Incorrect connection starter")
-        sel.unregister(conn)
+        SEL.unregister(conn)
         conn.close()
     else:
         print('verify STRIN : '+strin)
         conn.send(bytes("HI","utf-8"))
-        sel.modify(conn, selectors.EVENT_READ, main_loop)
-
+        SEL.modify(conn, selectors.EVENT_READ, main_loop)
 
 sock = socket.socket()
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 sock.bind(('localhost', 2101))
 sock.listen(100)
 sock.setblocking(False)
-sel.register(sock, selectors.EVENT_READ, accept)
+SEL.register(sock, selectors.EVENT_READ, accept)
 
 while True:
-    events = sel.select()
-    for key, mask in events:
+    evts = SEL.select()
+    for key, mask in evts:
         callback = key.data
         callback(key.fileobj, mask)
